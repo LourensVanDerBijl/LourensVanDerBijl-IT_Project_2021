263d6f58a38ebfdbd4faf07902e94ab5b97186b4
louis3546 <lourensvdbijl@gmail.com>
2021-10-24 13:04:55 +0200

    BootStrap
    Adding bootStrap for mobile devices and color schemes

--- cbhc-app/src/main.js
+++ cbhc-app/src/main.js
@@ -1,6 +1,7 @@
 import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import './assets/css/styles.css'
+import './assets/css/bootstrap.css'
 import App from './App.vue'
 import router from './router'
 
